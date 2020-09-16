6ce2f9e79fd6825dfa88e4a7d51d6d9f5f303835
Nick Nobile <nickcnobile@gmail.com>
2020-09-16 02:34:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,17 +9,21 @@ def using_sort(array)
 end
 
 def using_reverse(array)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 
 end
 
 def using_first(array)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 
 end
 
 def using_last(array)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 
 end
 
 def using_size(array)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 
 end
